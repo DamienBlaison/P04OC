ce833ff00efac6aef828d29181bd68d4298492a3
Damien Blaison <d.blaison@live.fr>
2019-01-30 14:09:13 +0100

    def
    def

--- index.php
+++ index.php
@@ -1,9 +1,13 @@
 <?php
+// starting session_start
 session_start();
 
+//require controllers
 require('./controller/controller.php');
 require('./controller/frontend/front_office.php');
 require('./controller/backend/backoffice.php');
+
+// test url to define required files and method
 
 if(isset($_GET['action'])){
 
@@ -11,12 +15,17 @@ if(isset($_GET['action'])){
       $rooter = $rooter[0];
 } else { $rooter = "";};
 
+//Instanciation object controller/backend/backoffice ->Backend
+
 $backend      = new Backend();
 
+// read param config with config Backend's method
 $array_config = $backend->config();
 
-$frontend     = new Frontend($array_config);
+//Instanciation object controller/frontend/frontoffice -> frontend
+$frontend     = new Frontend();
 
+// test if url form .../backoffice or not 
 switch ($rooter) {
 
       case 'backoffice':
